8dc98cda98549283b22b4dfe7457d62abdb7591b
Senorsen <senorsen.zhang@gmail.com>
2016-05-25 23:13:44 +0800

    Update build.sh: fix bugs

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 #
 
 set -e
+set -x
 
 DEBURL="http://s1.music.126.net/download/pc/netease-cloud-music_0.9.0-1_amd64.deb"
 DEBFILENAME="netease-cloud-music_0.9.0-1_amd64.deb"
@@ -15,12 +16,15 @@ NAME="netease-cloud-music"
 SEMIDIR="$NAME-$VERSION"
 FULLDIR="$SEMIDIR-$RELEASE.$ARCH"
 
-mkdir ~/rpmbuild/SOURCES
+mkdir -p ~/rpmbuild/SOURCES
 cd $(dirname $0)
 mkdir -p tmp
 cd tmp
+cp -rv ../netease-cloud-music.spec ./
+rm $DEBFILENAME -rf
 wget $DEBURL
 ar xf $DEBFILENAME
+tar xvf data.tar.xz
 mkdir -p $SEMIDIR
 mv usr $SEMIDIR/
 tar zcvf ${SEMIDIR}.tar.gz $SEMIDIR
